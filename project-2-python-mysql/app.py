c67f75b6c32568ff108742b40c064693eaf8c408
Vijay Kumar C M <121659233+vijaykumarcm1991@users.noreply.github.com>
2025-11-20 15:30:27 +0530

    Update app.py

--- project-2-python-mysql/app.py
+++ project-2-python-mysql/app.py
@@ -1,16 +1,28 @@
+import time
 import mysql.connector
+from mysql.connector import Error
 from flask import Flask
 
 app = Flask(__name__)
 
+def get_db_connection():
+    while True:
+        try:
+            conn = mysql.connector.connect(
+                host="db",
+                user="root",
+                password="root",
+                database="demo"
+            )
+            print("MySQL Connected!")
+            return conn
+        except Error as e:
+            print("MySQL not ready. Retrying in 3 seconds...")
+            time.sleep(3)
+
 @app.route("/")
 def home():
-    conn = mysql.connector.connect(
-        host="db",
-        user="root",
-        password="root",
-        database="demo"
-    )
+    conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS test(id INT PRIMARY KEY AUTO_INCREMENT, msg VARCHAR(255))")
     cursor.execute("INSERT INTO test(msg) VALUES('Hello from MySQL')")
